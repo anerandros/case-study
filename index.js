28290bd8941e609297e7fbd9826ebfbcd6b63f07
Andrea Martinelli <andrea.martinell@rai.it>
2022-10-26 15:50:29 +0200

    primo endpoint test

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
+});
+
+app.post("/user/save", (req, res) => {
+    const parsedBody = req.body;
+    if (parsedBody) {
+        // microservizio che salva l'utente
+        res.send(parsedBody);
+    } else {
+        res.end({
+            status: "KO",
+            message: "Error in input",
+        });
+    }
 });
 
 app.listen(port, () => {
