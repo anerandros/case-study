b688b6e04fa2e67a7e0d5481067cbabff92c7e41
Andrea Martinelli <andrea.martinell@rai.it>
2022-11-03 11:45:33 +0100

    rimossi pacchetti

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -20,11 +20,6 @@ module.exports = {
                     loader: "ts-loader",
                 },
             },
-            {
-                test: /\.js$/,
-                loader: "babel-loader",
-                exclude: /(node_modules)/,
-            },
         ],
     },
 };
