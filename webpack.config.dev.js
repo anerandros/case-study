d93d1b7262c793ccd65c54fa639f697e0912fd23
Andrea Martinelli <andrea.martinell@rai.it>
2022-11-03 11:14:14 +0100

    primo aggiornamento typescript

---
+++ webpack.config.dev.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+    entry: {
+        creditagricole: "./models/creditagricole.class.ts",
+    },
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, "dev"),
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "ts-loader",
+                },
+            },
+        ],
+    },
+};
